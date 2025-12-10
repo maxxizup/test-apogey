90b0a7878efc4b90d72e6855f03542e184c36c31
Predator22876 <alienchose25@gmail.com>
2025-12-11 01:46:48 +1100

    [backend] сделал get ручку

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,19 @@
-print("Hello world")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins= ["http://localhost:3000/"]
+)
+
+film = {
+    "name": "Oblasty_tymy",
+    "length": 300
+}
+
+@app.get("/")
+async def get_film():
+    return film
